19bf7354b97bfbb34148c3a5dbbb184eee13ce52
pompopo <pompopo@gmail.com>
2019-06-07 18:44:25 +0900

    initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import UIKit
+import PlaygroundSupport
+
+struct ContentView : View {
+    var body: some View {
+        Text("pompopo")
+    }
+}
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: ContentView())
